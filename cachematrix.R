0d94299d33d3951f7fbff595a996a4b8851db7ca
edemdosko <eddeym@gmail.com>
2016-01-24 01:36:36 -0800

    Committing Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+###################################################################
+#The following two functions are meant to be an application of the 
+#the lexical scpoing rules in R. One will create a matrix that ca-
+#ches its inverse--makeCacheMatrix, and the other will compute the
+#inverse of the matrix
+###################################################################
 
-## Write a short comment describing this function
 
+## function that creates a matrix a caches it
 makeCacheMatrix <- function(x = matrix()) {
-
+      s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x        
+        set_solve <- function(solve) s <<- solve
+        get_solve <- function() s
+        list(set = set, get = get,
+             set_solve = set_solve,
+             get_solve = get_solve)
 }
 
 
-## Write a short comment describing this function
-
+##function to compute the inverse of the matrix returned by makeCacheMatrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        s <- x$get_solve()
+        if(!is.null(s)) {
+                message("getting cached matrix")
+                return(s)
+        }
+        my_matrix <- x$get()
+        s <- solve(my_matrix)
+        x$set_solve(s)
+        s
 }
